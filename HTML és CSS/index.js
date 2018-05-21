91af0c0030e9e8b3ee0ce3b75a7f3b67725a9685
Mészáros Martin <hdsektor38@gmail.com>
2018-05-21 17:54:06 +0200

    asd

---
+++ HTML és CSS/index.js
@@ -0,0 +1,19 @@
+function name() {
+
+}
+
+function password() {
+
+}
+
+function enter(){
+    alert("Hello World!")
+    open("http://www.pannoniait.me")
+}
+
+setTimeout(function () {
+    alert("Ébresztő!")
+}, 600000);
+function functionName() {
+
+}
