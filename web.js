2f7f8ded387f46b4b079e0284c815fd94d92397c
sektor338 <37712920+sektor338@users.noreply.github.com>
2018-05-28 13:21:27 +0200

    Add files via upload

--- web.js
+++ web.js
@@ -1,3 +1,23 @@
 function tolt() {
-
+  setInterval(ido_lekerdezes, 1000);
+}
+function ido_lekerdezes() {
+  var datumido = new Date();
+  var nap = datumido.getDate();
+  var honap = datumido.getMonth();
+  var ev = datumido.getFullYear();
+  var ora = datumido.getHours();
+  var perc = datumido.getMinutes();
+  var mp = datumido.getSeconds();
+  document.getElementsByClassName('pontosido').innerHTML = ev + "." + honap + "." + nap + "." + ora + "." + perc + "." + mp;
+}
+function bekuldes() {
+  var nev = document.getElementsByClassName('nev_mezo').value;
+  var uzenet = document.getElementsByClassName('uzenet_mezo').value;
+  var datumido = new Date();
+  var ora = datumido.getHours();
+  var perc = datumido.getMinutes();
+  var mp = datumido.getSeconds();
+  document.getElementsByClassName('cseveges_mezo').value += nev + "[" + ora + ":" + perc + ":" + mp + "]" + ": " + uzenet + "\n";
+  alert('Üzenet elküldve!');
 }
