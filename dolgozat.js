ee485933bb899fe4cb8883af1fec093a52c7f982
sektor338 <37712920+sektor338@users.noreply.github.com>
2018-06-04 11:29:49 +0200

    év végi doga

---
+++ dolgozat.js
@@ -0,0 +1,6 @@
+var nev = document.getElementsByClassName("nev_mezo").value;
+var eletkor = document.getElementsByClassName("eletkor_mezo").value;
+
+function bekuld() {
+    alert("Üzenet elküldve! " + nev + eletkor);
+}
