5002e32e1fe7a7f8931c94fc2795f0ddb45ae02f
sektor338 <37712920+sektor338@users.noreply.github.com>
2018-06-11 11:14:37 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,5 @@
+var number = 0;
+document.getElementsByClassName('egyenlo_gombok').addEventListener("click",function(){
+    number = 1;
+    document.getElementsByClassName("kijelzo").innerHTML=number;
+}
